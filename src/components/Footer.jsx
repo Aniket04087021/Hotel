3127d9fe03551ca053f27887e9d2bc1e6349fdf6
Aniket04080218 <gupta.aniket0408@gmail.com>
2024-10-20 13:09:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ANIII</div>
+          <div className="right">
+            <p>601 Shelar Residency , Kalyan</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ANIII</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By ANIII.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
